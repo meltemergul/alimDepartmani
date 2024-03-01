776b4ef5b53020b0e4748f010869b8fb6023b06c
Meltem Ergül <95168323+meltemergul@users.noreply.github.com>
2024-03-01 12:01:34 +0300

    Proje dosyası ekle.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace alimDepartmani.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
